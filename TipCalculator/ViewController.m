4fdedd5721ef2d33251a8ef810c501c24ca08592
Jimmy Hoang <hoangjimmy94@gmail.com>
2017-06-09 11:27:57 -0700

    Add tip calculation functionality with slider and user input

--- TipCalculator/ViewController.m
+++ TipCalculator/ViewController.m
@@ -9,6 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *billAmountTextField;
+@property (weak, nonatomic) IBOutlet UILabel *tipAmountLabel;
+@property (weak, nonatomic) IBOutlet UILabel *tipPercentageLabel;
+@property (weak, nonatomic) IBOutlet UISlider *tipSlider;
 
 @end
 
@@ -25,5 +29,17 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)adjustTipPercentage:(id)sender {
+    
+    self.tipPercentageLabel.text = [NSString stringWithFormat:@"%.2f%%",self.tipSlider.value];
+    float billAmount = self.billAmountTextField.text.floatValue;
+    float tipPercentage = self.tipSlider.value;
+    float tip = (billAmount * tipPercentage)/100;
+    self.tipAmountLabel.text = [NSString stringWithFormat:@"Your tip will be $%.2f",tip];
+}
+- (IBAction)tapGesture:(UITapGestureRecognizer*)sender {
+    
+    [self.view endEditing:YES];
+}
 
 @end
